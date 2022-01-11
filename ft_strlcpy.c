c6c256737eaa16bdcf9515bf8863ece9c46527ed
user42 <psaumet@student.42.fr>
2022-01-11 14:29:55 +0100

    Adding ft_strlcpy.c and ft_strlen.c, need to links ft_strlen with other

---
+++ ft_strlcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 11:20:06 by psaumet           #+#    #+#             */
+/*   Updated: 2019/11/22 13:33:53 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Copy strings, copies src to dst.
+**	Returns the total length of the string.
+*/
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = ft_strlen(src);
+	j = 0;
+	if (dst == NULL)
+		return (0);
+	if (src == NULL)
+		return (0);
+	if (!dstsize)
+		return (i);
+	while (src[j] && j < (dstsize - 1))
+	{
+		dst[j] = src[j];
+		j++;
+	}
+	dst[j] = '\0';
+	return (i);
+}
